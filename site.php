9db1fffdaa2cffe52c6e00eb7b4877f339037627
Katie Duane <katiejduane@gmail.com>
2020-06-08 22:13:23 -0400

    first commit

---
+++ site.php
@@ -0,0 +1,23 @@
+<!-- just some basics -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php basics</title>
+</head>
+<body>
+
+    <?php 
+        echo "<h2>katie's world</h2>";
+        echo "<hr>";
+        echo "<p>why the hell isn't this working?</p>";
+        echo "<p>ah there we go</p>";
+    ?>
+    
+</body>
+</html>
+    
+</body>
+</html>
